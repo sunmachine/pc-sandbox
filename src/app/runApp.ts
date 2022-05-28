0de52ff8cccc4e965b79287011a26a07919659a9
Dan Peavey <danpeavey@gmail.com>
2022-05-28 11:21:35 -0700

    Adding mouse input controls to runApp.

--- src/app/runApp.ts
+++ src/app/runApp.ts
@@ -2,7 +2,12 @@ import * as pc from "playcanvas";
 import { MainScene } from "./scene/MainScene";
 
 export function runApp(canvas: Element) {
-  const app = new pc.Application(canvas);
+  const app = new pc.Application(canvas, {
+    elementInput: new pc.ElementInput(canvas),
+    mouse: new pc.Mouse(canvas),
+    touch: "ontouchstart" in window ? new pc.TouchDevice(canvas) : undefined,
+    keyboard: new pc.Keyboard(window),
+  });
 
   app.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
   app.setCanvasResolution(pc.RESOLUTION_AUTO);
