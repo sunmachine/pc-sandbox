87aaf8c62be5f06ed90795802319edb6ee50f838
Dan Peavey <danpeavey@gmail.com>
2022-05-28 08:16:13 -0700

    Bootstrapping Playcanvas rendering to vue3 components.

---
+++ src/components/PlaycanvasComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { runApp } from "@/engine/runApp";
+import { ref, onMounted } from "vue";
+
+const application = ref(null);
+onMounted(() => {
+  runApp(application.value as unknown as Element);
+});
+</script>
+
+<template>
+  <canvas ref="application" id="playcanvas" />
+</template>
