cfdbf4f6ecc9695af2c8716dae4a8e2a5666fa2f
Dan Peavey <danpeavey@gmail.com>
2022-05-29 00:17:57 -0700

    Re-implementing SpinningCube with parameterized initialization args.

--- src/app/actors/SpinningCubeActor.ts
+++ src/app/actors/SpinningCubeActor.ts
@@ -1,18 +1,30 @@
 import * as pc from "playcanvas";
 import { SceneActor } from "./SceneActor";
 
-export class SpinningCubeActor extends SceneActor {
+export class SpinningCubeActor extends SceneActor<pc.Material> {
   box?: pc.Entity;
 
-  init(): this {
+  private readonly _cubeOpts = {
+    halfExtents: new pc.Vec3(0.5, 0.1, 0.5),
+    widthSegments: 2,
+    lengthSegments: 2,
+    heightSegments: 2,
+    calculateTangents: true,
+  };
+
+  init(material: pc.Material): this {
     const app = this.scene.app;
     this.box = new pc.Entity("cube");
-    this.box.addComponent("model", {
-      type: "box",
+
+    const mesh = pc.createBox(app.graphicsDevice, this._cubeOpts);
+    const meshInstance = new pc.MeshInstance(mesh, material);
+
+    this.box.addComponent("render", {
+      meshInstances: [meshInstance],
     });
     this.parent.addChild(this.box);
 
-    return super.init();
+    return super.init(material);
   }
 
   update(dt: number): void {
