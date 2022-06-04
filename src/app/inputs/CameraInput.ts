c9541f0af2fc35a9027ec04913df43122f14723a
Dan Peavey <danpeavey@gmail.com>
2022-06-04 19:07:56 -0400

    Implementing orbit cam, and temporarily disabling all other inputs.

--- src/app/inputs/CameraInput.ts
+++ src/app/inputs/CameraInput.ts
@@ -1,13 +1,17 @@
 import * as pc from "playcanvas";
 import type { CameraActor } from "../actors/CameraActor";
 import { SceneActor } from "../actors/SceneActor";
+import { cartesianToSpherical } from "../math/SphericalCoords";
 import { Direction, hasDirection } from "./Direction";
 
 export class CameraInput extends SceneActor<CameraActor> {
   camera?: CameraActor;
 
   private readonly panSpeedScalar = 0.01;
+  private readonly orbitSpeedScalar = 0.01;
   private readonly moveSpeedScalar = 1;
+  private readonly zoomSpeedScalar = 0.1;
+
   private currentMoveDir: Direction = Direction.NONE;
   private pressedKeys = new Set<number>();
 
@@ -29,9 +33,11 @@ export class CameraInput extends SceneActor<CameraActor> {
   init(camera?: CameraActor): this {
     this.camera = camera;
 
-    app.mouse.on("mousemove", this.onMouseMove());
-    app.keyboard.on("keydown", this.onKeyDown());
-    app.keyboard.on("keyup", this.onKeyUp());
+    app.mouse.on("mousemove", (e) => this.onMouseMove(e));
+    app.mouse.on("mousewheel", (e) => this.onMouseWheel(e));
+    // TODO: Refactor to work with Orbit Cam.
+    // app.keyboard.on("keydown", (e) => this.onKeyDown(e));
+    // app.keyboard.on("keyup", (e) => this.onKeyUp(e));
 
     this.camera?.entity?.lookAt(pc.Vec3.ZERO);
 
@@ -55,58 +61,98 @@ export class CameraInput extends SceneActor<CameraActor> {
     }
   }
 
-  private onMouseMove(): pc.HandleEventCallback {
-    return (evt) => {
-      if (app.mouse.isPressed(pc.MOUSEBUTTON_LEFT)) {
-        if (evt.dx && evt.dy) {
-          const pos = this.camera?.entity?.getPosition();
-          if (pos) {
-            pos.x -= evt.dx * this.panSpeedScalar;
-            pos.y += evt.dy * this.panSpeedScalar;
-            this.camera?.entity?.setPosition(pos);
-          }
-        }
-      }
-    };
-  }
+  private onMouseWheel(evt: pc.MouseEvent) {
+    if (evt) {
+      this.zoom(evt);
 
-  private onKeyDown(): pc.HandleEventCallback {
-    return (evt) => {
-      this.keyMapping.forEach((map) => {
-        if (
-          app.keyboard.isPressed(map.key) &&
-          !this.pressedKeys.has(map.key) &&
-          hasDirection(this.currentMoveDir, map.direction) === false
-        ) {
-          this.pressedKeys.add(map.key);
-          this.currentMoveDir += map.direction;
-        }
-      });
-
-      if (this.currentMoveDir > Direction.NONE && !this.isMoving) {
-        this.isMoving = true;
+      if (evt.wheelDelta) {
         evt.event.preventDefault();
       }
-    };
+    }
   }
 
-  private onKeyUp(): pc.HandleEventCallback {
-    return (evt) => {
-      this.keyMapping.forEach((map) => {
-        if (
-          !app.keyboard.isPressed(map.key) &&
-          this.pressedKeys.has(map.key) &&
-          hasDirection(this.currentMoveDir, map.direction)
-        ) {
-          this.pressedKeys.delete(map.key);
-          this.currentMoveDir -= map.direction;
-        }
-      });
-
-      if (this.currentMoveDir === Direction.NONE && this.isMoving) {
-        this.isMoving = false;
-        evt.event.preventDefault();
+  private onMouseMove(evt: pc.MouseEvent) {
+    if (app.mouse.isPressed(pc.MOUSEBUTTON_LEFT)) {
+      if (app.keyboard.isPressed(pc.KEY_SHIFT)) {
+        // TODO: Refactor to work with Orbit Cam.
+        // this.pan(evt);
+      } else {
+        this.orbit(evt);
       }
-    };
+    }
+  }
+
+  private onKeyDown(evt: pc.KeyboardEvent) {
+    this.keyMapping.forEach((map) => {
+      if (
+        app.keyboard.isPressed(map.key) &&
+        !this.pressedKeys.has(map.key) &&
+        hasDirection(this.currentMoveDir, map.direction) === false
+      ) {
+        this.pressedKeys.add(map.key);
+        this.currentMoveDir += map.direction;
+      }
+    });
+
+    if (this.currentMoveDir > Direction.NONE && !this.isMoving) {
+      this.isMoving = true;
+      evt.event.preventDefault();
+    }
+  }
+
+  private onKeyUp(evt: pc.KeyboardEvent) {
+    this.keyMapping.forEach((map) => {
+      if (
+        !app.keyboard.isPressed(map.key) &&
+        this.pressedKeys.has(map.key) &&
+        hasDirection(this.currentMoveDir, map.direction)
+      ) {
+        this.pressedKeys.delete(map.key);
+        this.currentMoveDir -= map.direction;
+      }
+    });
+
+    if (this.currentMoveDir === Direction.NONE && this.isMoving) {
+      this.isMoving = false;
+      evt.event.preventDefault();
+    }
+  }
+
+  private orbit(evt: pc.MouseEvent) {
+    if (evt.dx && evt.dy) {
+      const pos = this.camera?.entity?.getPosition();
+      if (pos) {
+        const scoords = cartesianToSpherical(pos);
+        scoords.polar += evt.dx * this.orbitSpeedScalar;
+        scoords.elevation += evt.dy * this.orbitSpeedScalar;
+        scoords.toCartesian(pos);
+        this.camera?.entity?.setPosition(pos);
+        this.camera?.entity?.lookAt(pc.Vec3.ZERO);
+      }
+    }
+  }
+
+  private zoom(evt: pc.MouseEvent) {
+    if (evt.wheelDelta) {
+      const pos = this.camera?.entity?.getPosition();
+      if (pos) {
+        const scoords = cartesianToSpherical(pos);
+        scoords.radius += evt.wheelDelta * this.zoomSpeedScalar;
+        scoords.toCartesian(pos);
+        this.camera?.entity?.setPosition(pos);
+        this.camera?.entity?.lookAt(pc.Vec3.ZERO);
+      }
+    }
+  }
+
+  private pan(evt: pc.MouseEvent) {
+    if (evt.dx && evt.dy) {
+      const pos = this.camera?.entity?.getPosition();
+      if (pos) {
+        pos.x -= evt.dx * this.panSpeedScalar;
+        pos.y += evt.dy * this.panSpeedScalar;
+        this.camera?.entity?.setPosition(pos);
+      }
+    }
   }
 }
