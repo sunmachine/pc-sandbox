dd01b1fa21fb156ceef82a068cf4e7ce774d61bd
Dan Peavey <danpeavey@gmail.com>
2022-06-05 18:55:38 -0400

    Implementing basic procedural-skybox.

--- src/app/scene/MainScene.ts
+++ src/app/scene/MainScene.ts
@@ -1,9 +1,9 @@
 import * as pc from "playcanvas";
 import { CameraActor } from "../actors/CameraActor";
-import { GroundPlaneActor } from "../actors/GroundPlaneActor";
 import { LightActor } from "../actors/LightActor";
 import { SpinningCubeActor } from "../actors/SpinningCubeActor";
 import { CameraInput } from "../inputs/CameraInput";
+import { Skybox } from "../skybox/Skybox";
 import { Scene } from "./Scene";
 
 export class MainScene extends Scene {
@@ -12,11 +12,15 @@ export class MainScene extends Scene {
     const root = app.root;
     const defaultMaterial = new pc.StandardMaterial();
 
+    // Setup Skybox.
+    const { envAtlas, skybox } = new Skybox().generate();
+    this.scene.skybox = skybox;
+    this.scene.envAtlas = envAtlas;
+
     // Create base scene actors.
     const camera = new CameraActor(this, root).init();
     new LightActor(this, root).init();
     new SpinningCubeActor(this, root).init(defaultMaterial);
-    new GroundPlaneActor(this, root).init(defaultMaterial);
 
     // Handle scene inputs.
     new CameraInput(this, root).init(camera);
---
+++ src/app/skybox/Skybox.ts
@@ -0,0 +1,41 @@
+import * as pc from "playcanvas";
+
+export class Skybox {
+  private readonly _x = 128;
+  private readonly _y = 128;
+
+  generate(): { envAtlas: pc.Texture; skybox: pc.Texture } {
+    const t = new pc.Texture(app.graphicsDevice, {
+      format: pc.PIXELFORMAT_R8_G8_B8,
+      width: this._x,
+      height: this._y,
+      addressU: pc.ADDRESS_CLAMP_TO_EDGE,
+      addressV: pc.ADDRESS_CLAMP_TO_EDGE,
+    });
+
+    const pixels = t.lock();
+    let count = 0;
+    for (let i = 0; i < this._x; i++) {
+      for (let j = 0; j < this._y; j++) {
+        const c = 256 - i * (256 / this._y);
+        pixels[count++] = c;
+        pixels[count++] = c;
+        pixels[count++] = c;
+      }
+    }
+    t.unlock();
+
+    return this.initFromTexture(t);
+  }
+
+  private initFromTexture(env: pc.Texture) {
+    const skybox = pc.EnvLighting.generateSkyboxCubemap(env);
+    const lighting = pc.EnvLighting.generateLightingSource(env);
+
+    // The second options parameter should not be necessary but the TS declarations require it for now
+    const envAtlas = pc.EnvLighting.generateAtlas(lighting, {});
+    lighting.destroy();
+
+    return { envAtlas, skybox };
+  }
+}
