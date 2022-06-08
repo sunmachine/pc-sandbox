905bda3af5e5a58335c72fbdb6475733752c5829
Dan Peavey <danpeavey@gmail.com>
2022-06-08 13:57:41 -0400

    Adding helpers for degrees and radians.

--- src/app/types/Radians.ts
+++ src/app/types/Radians.ts
@@ -1,2 +1,12 @@
+import { math } from "playcanvas";
+
 export type Radian = number;
 export type Degrees = number;
+
+export function degToRad(degrees: number) {
+  return degrees * math.DEG_TO_RAD;
+}
+
+export function radToDeg(radians: Radian) {
+  return radians * math.RAD_TO_DEG;
+}
