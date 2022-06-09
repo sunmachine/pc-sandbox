6f47be3adcceaa0d0062a10a0dd39486385c5a9c
Dan Peavey <danpeavey@gmail.com>
2022-06-09 12:29:34 -0400

    Annotating Vector aliases as Vectorlike.

--- src/app/types/Vectors.ts
+++ src/app/types/Vectors.ts
@@ -1,5 +1,6 @@
 import type * as pc from "playcanvas";
+import type { Vectorlike } from "./Vectorlike";
 
-export type Vector2 = pc.Vec2;
-export type Vector3 = pc.Vec3;
-export type Vector4 = pc.Vec4;
+export type Vector2 = pc.Vec2 & Vectorlike<pc.Vec2>;
+export type Vector3 = pc.Vec3 & Vectorlike<pc.Vec3>;
+export type Vector4 = pc.Vec4 & Vectorlike<pc.Vec4>;
