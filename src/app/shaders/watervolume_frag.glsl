8ac32e8a87edf3e42a141695bfc44e44c37e4f8d
Dan Peavey <danpeavey@gmail.com>
2022-07-10 23:45:36 -0400

    Changing volume shader to a checkboard for debugging.

--- src/app/shaders/watervolume_frag.glsl
+++ src/app/shaders/watervolume_frag.glsl
@@ -3,8 +3,13 @@ precision highp float;
 varying vec2 uv0;
 uniform float time;
 
+const vec2 u_repeat = vec2(2, 2); 
+
 void main(void)
 {
-    vec4 color = vec4(uv0.x, uv0.y, 1.0 - time * (uv0.r + uv0.g), 0.5);
+
+    float v = mod(dot(vec2(1.0), step(vec2(0.5), fract(uv0 * u_repeat))), 2.0);
+    vec4 color = vec4(v, v, v, 0.5);
+
     gl_FragColor = color;
 }
