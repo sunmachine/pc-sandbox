0c59ff2a13379e7fd7db3100123188a9b3114495
Dan Peavey <danpeavey@gmail.com>
2022-07-10 23:04:35 -0400

    Water actor setup to render two separate entities.

--- src/app/actors/water/Water.ts
+++ src/app/actors/water/Water.ts
@@ -2,28 +2,54 @@ import { Entity, MeshInstance } from "playcanvas";
 import * as pc from "playcanvas";
 import { Actor } from "../Actor";
 import { WaterMaterial } from "./WaterMaterial";
+import { addLayer } from "@/app/utils/Layers";
+
+export const LAYER_WATERVOLUME = 128;
+export const LAYER_WATERSURFACE = 129;
 
 export class Water extends Actor {
-  private material: WaterMaterial;
+  private volumeMat: WaterMaterial;
+  private surfaceMat: WaterMaterial;
 
   constructor(root: pc.Entity) {
     super(root);
 
     const mesh = pc.createBox(pc.app.graphicsDevice);
-    this.material = new WaterMaterial();
-    const meshInstance = new MeshInstance(mesh, this.material);
-    const entity = new Entity();
 
-    entity.addComponent("render", {
-      meshInstances: [meshInstance],
+    this.volumeMat = new WaterMaterial("volume");
+    this.surfaceMat = new WaterMaterial("surface");
+
+    this.volumeMat.cull = pc.CULLFACE_FRONT;
+    this.surfaceMat.cull = pc.CULLFACE_BACK;
+    this.volumeMat.blendType = this.surfaceMat.blendType = pc.BLEND_NORMAL;
+
+    const volumeMesh = new MeshInstance(mesh, this.volumeMat);
+    const surfaceMesh = new MeshInstance(mesh, this.surfaceMat);
+
+    const parent = new Entity();
+    const volumeEntity = new Entity();
+    const surfaceEntity = new Entity();
+    parent.addChild(volumeEntity);
+    parent.addChild(surfaceEntity);
+
+    volumeEntity.addComponent("render", {
+      meshInstances: [volumeMesh],
     });
-    this.root.addChild(entity);
-    entity.setPosition(0, 0.5, 0);
+    surfaceEntity.addComponent("render", {
+      meshInstances: [surfaceMesh],
+    });
 
-    this.entity = entity;
+    volumeEntity.render?.layers;
+
+    this.root.addChild(parent);
+    parent.setPosition(0, 0.5, 0);
+
+    this.entity = parent;
   }
 
   update(dt: number) {
-    this.material.updateParameters(dt);
+    this.volumeMat.updateParameters(dt);
+    this.surfaceMat.updateParameters(dt);
+  }
   }
 }
