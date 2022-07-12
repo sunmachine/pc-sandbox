b881bb73eddde59f9e108de79aa1155ca18e3f84
Dan Peavey <danpeavey@gmail.com>
2022-07-12 01:15:43 -0400

    Foggin' up the cube.

--- src/app/shaders/watervolume_frag.glsl
+++ src/app/shaders/watervolume_frag.glsl
@@ -1,14 +1,17 @@
 precision highp float;
 
-varying vec2 uv0;
+const vec2 u_repeat = vec2(2.0, 2.0); 
 uniform float time;
 
-const vec2 u_repeat = vec2(2, 2); 
+varying vec3 local_position;
+varying vec2 uv0;
 
 void main(void)
 {
+    float d = (local_position.y + 0.5) / 2.0;
+    d *= d * d;
     float v = mod(dot(vec2(1.0), step(vec2(0.5), fract(uv0 * u_repeat))), 2.0);
-    vec4 color = vec4(v, v, v, 0.25);
+    vec4 color = vec4(v, v, v, d);
 
     gl_FragColor = color;
 }
--- src/app/shaders/watervolume_vert.glsl
+++ src/app/shaders/watervolume_vert.glsl
@@ -6,10 +6,12 @@ attribute vec2 a_uv0;
 uniform mat4 matrix_model;
 uniform mat4 matrix_viewProjection;
 
+varying vec3 local_position;
 varying vec2 uv0;
 
 void main(void)
 {
     uv0 = a_uv0;
+    local_position = a_position; 
     gl_Position = matrix_viewProjection * matrix_model * vec4(a_position, 1.0);
 }
