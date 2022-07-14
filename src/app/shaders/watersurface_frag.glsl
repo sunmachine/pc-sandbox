251f120dacb25b9e44a10fd1f0080979cd2c3472
Dan Peavey <danpeavey@gmail.com>
2022-07-14 19:07:45 -0400

    Removing time value for now, and moving to depth^2.

--- src/app/shaders/watersurface_frag.glsl
+++ src/app/shaders/watersurface_frag.glsl
@@ -5,6 +5,6 @@ uniform float time;
 
 void main(void)
 {
-    vec4 color = vec4(uv0.x, uv0.y, time * (uv0.x + uv0.y), 0.5);
+    vec4 color = vec4(uv0.x, uv0.y, (uv0.x + uv0.y), 0.5);
     gl_FragColor = color;
 }
--- src/app/shaders/watervolume_frag.glsl
+++ src/app/shaders/watervolume_frag.glsl
@@ -9,7 +9,7 @@ varying vec2 uv0;
 void main(void)
 {
     float d = (local_position.y + 0.5) / 2.0;
-    d *= d * d;
+    d *= d;
     float v = mod(dot(vec2(1.0), step(vec2(0.5), fract(uv0 * u_repeat))), 2.0) * d;
     vec4 color = vec4(v, v, v, 1.0 - d);
 
