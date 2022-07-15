05c2172c6f4dd353cba34a1180425521a2e3b807
Dan Peavey <danpeavey@gmail.com>
2022-07-14 23:59:50 -0400

    Adding new shared Serializable and Transform interfaces.

---
+++ src/shared/Serializables.ts
@@ -0,0 +1,3 @@
+export type Number2 = [number, number];
+export type Number3 = [number, number, number];
+export type Number4 = [number, number, number, number];
---
+++ src/shared/Transform.ts
@@ -0,0 +1,7 @@
+import type { Number3 } from "./Serializables";
+
+export interface Transform {
+  position: Number3;
+  rotation: Number3;
+  scale: Number3;
+}
